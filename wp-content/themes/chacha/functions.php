1c0be0d640c917210d5285ef2530d2132c515082
David Dufour Morin <ddmorin@cha-cha.ca>
2023-07-13 11:04:22 -0400

    Copyright and CookieYes Corrections

--- wp-content/themes/chacha/functions.php
+++ wp-content/themes/chacha/functions.php
@@ -8,18 +8,27 @@
  * @since 1.0.0
  */
 
-define( 'theme_version', '1.0.0' );
+define('theme_version', '1.0.0');
 
-include get_template_directory().'/inc/theme.php';
+include get_template_directory() . '/inc/theme.php';
 
-add_action( 'init', function(){
-    if( class_exists('ACF') ){
+add_action('init', function () {
+    if (class_exists('ACF')) {
         acf_add_options_page(array(
-            'page_title' 	=> __( 'Options du thème', 'chacha' ),
-            'menu_title'	=> __( 'Options du thème', 'chacha' ),
-            'menu_slug' 	=> 'theme-options',
-            'capability'	=> 'manage_options',
-            'redirect'		=> false
+            'page_title' => __('Options du thème', 'chacha'),
+            'menu_title' => __('Options du thème', 'chacha'),
+            'menu_slug' => 'theme-options',
+            'capability' => 'manage_options',
+            'redirect' => false,
         ));
     }
-} );
+});
+
+// function that runs when shortcode is called
+function add_year_to_wysiwyg()
+{
+    // Output needs to be return
+    return date('Y');
+}
+// register shortcode
+add_shortcode('Year', 'add_year_to_wysiwyg');
